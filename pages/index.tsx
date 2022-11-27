d540c60db0e21a52ce2eb0a531aba1748a870dab
Max Mohammadi <maxmohammadi@gmail.com>
2022-11-26 20:15:54 -0800

    add preview

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import SectionStats from "../dark/Sections/SectionStats";
 import SectionUsers from "../dark/Sections/SectionUsers";
 import styles from "./../styles/dark/Home.module.css";
 import favicon from "../public/images/favicon.ico";
+import preview from "../public/images/preview.png";
 
 const DarkHome: NextPage = () => {
   return (
@@ -15,6 +16,12 @@ const DarkHome: NextPage = () => {
       <Head>
         <title>Seam Money</title>
         <link rel="shortcut icon" href={favicon.src} type="image/x-icon" />
+        <meta property="og:url" content="https://seam.money"/>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Seam Money" />
+        <meta name="twitter:card" content="summary" />
+        <meta property="og:description" content="Distributed Yield and Staking Solution on Aptos" />
+        <meta property="og:image" content={preview.src} />
       </Head>
       <Navigation />
       <Heading />
