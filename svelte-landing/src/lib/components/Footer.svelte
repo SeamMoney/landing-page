f557c17a762d9bb6fa98937261a550a49a26ff41
Max Mohammadi <maxmohammadi@gmail.com>
2022-12-24 22:52:05 -0800

    add

---
+++ svelte-landing/src/lib/components/Footer.svelte
@@ -0,0 +1,12 @@
+<footer class="pt-12 pb-10">
+	<div class="flex flex-col mx-auto max-w-7xl">
+		<div class="flex justify-between border-t-[3px] border-dashed border-white py-5 px-8 text-gray-400">
+			<p>© Seam Labs, Inc. 2022, All Rights Reserved.</p>
+			<div class="flex space-x-4">
+				<img src="/images/socials/twitter.svg" width={26} height={16} alt="Twitter Icon" />
+				<img src="/images/socials/facebook.svg" width={26} height={16} alt="Facebook Icon" />
+				<img src="/images/socials/google.svg" width={26} height={16} alt="Google Icon" />
+			</div>
+		</div>
+	</div>
+</footer>
---
+++ svelte-landing/src/routes/__layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import '../app.css'
+	import '@fontsource/inter/300.css'
+	import '@fontsource/inter/400.css'
+	import '@fontsource/inter/600.css'
+	import '@fontsource/inter/700.css'
+	import '@fontsource/inter/800.css'
+	import Navbar from '$lib/components/Navbar.svelte'
+	import Footer from '$lib/components/Footer.svelte'
+</script>
+
+<div class="dope-background overflow-x-hidden text-white bg-black">
+	<Navbar />
+	<slot />
+	<Footer />
+</div>
+
+<style lang="postcss" global>
+	button {
+		@apply px-[18px] py-[10px] rounded-[100px] font-bold;
+		@apply text-primary bg-white;
+	}
+	button.primary {
+		@apply text-white bg-primary;
+		@apply px-8;
+	}
+</style>
